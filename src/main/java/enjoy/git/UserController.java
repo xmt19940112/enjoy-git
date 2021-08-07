7d62494f1bdc57aadcef52ecd7767eb36202f8fa
zhourui <841433605@qq.com>
2021-08-07 19:37:23 +0800

    update

--- src/main/java/enjoy/git/UserController.java
+++ src/main/java/enjoy/git/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
     @RequestMapping("/hello")
+    
     public Object sayHello() {
         return "hello-zhourui!";
     }

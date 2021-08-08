9dc4ca2e2eef39ddc9542651db3410d7f5a700f3
zhourui <841433605@qq.com>
2021-08-08 16:34:40 +0800

    更新controller

--- src/main/java/enjoy/git/App.java
+++ src/main/java/enjoy/git/App.java
@@ -10,6 +10,7 @@ public class App extends SpringBootServletInitializer  {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(App.class, args);
     }
+    
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(App.class);
